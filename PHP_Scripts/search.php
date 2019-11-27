125a670913f61ee1348c485aa105303e0a2d465a
Miguel Pinto <pt.miguel99@gmail.com>
2019-11-27 16:08:35 +0000

    Fixed database error

--- PHP_Scripts/search.php
+++ PHP_Scripts/search.php
@@ -1,18 +1,15 @@
 <?php
     include_once "../Database/connection.php";
 
-    function getSearch(){
+    function getSearch($location){
         global $db;
 		$stmt = $db->prepare('
-			SELECT news.*, users.*, COUNT(comments.id) AS comments
-			FROM news JOIN
-	     		users USING (username) LEFT JOIN
-	     		comments ON comments.news_id = news.id
-			GROUP BY news.id, users.     
-			ORDER BY published DESC
-	 	');
-		$stmt->execute();
-		$articles = $stmt->fetchAll();
+			SELECT *
+			FROM Property
+			WHERE location = ?');
+		$stmt->execute(array($location));
+        
+        $articles = $stmt->fetchAll();
 		return $articles;
     }
 ?>
