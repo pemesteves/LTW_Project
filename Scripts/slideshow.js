ac915cf9a115d928d8593552f5008b853c88ee14
Nuno <up201706162@fe.up.pt>
2019-11-27 15:53:38 +0000

    CSS stuff

--- Scripts/slideshow.js
+++ Scripts/slideshow.js
@@ -26,3 +26,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+//Event listeners
+//Previous and next arrows
+let prev = document.getElementsByClassName("prev")[0]
+prev.addEventListener("click", function(){plusSlides(-1)})
+let next = document.getElementsByClassName("next")[0]
+next.addEventListener("click", function(){plusSlides(1)})
+//Current number of the slide
+let current_slide1 = document.getElementsByClassName("dot")[0]
+current_slide1.addEventListener("click", function(){currentSlide(1)})
+let current_slide2 = document.getElementsByClassName("dot")[1]
+current_slide2.addEventListener("click", function(){currentSlide(2)})
+let current_slide3 = document.getElementsByClassName("dot")[2]
+current_slide3.addEventListener("click", function(){currentSlide(3)})
