fd3c075e2658e9ea6b89270f7b041789c38c7bca
Nuno <up201706162@fe.up.pt>
2019-11-30 19:11:38 +0000

    Improved login style

--- Pages/login.php
+++ Pages/login.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" type="text/css" href="../Styles/layout.css">
     <link rel="stylesheet" type="text/css" href="../Styles/style.css">
     <link rel="stylesheet" type="text/css" href="../Styles/search_bar.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/login.css">
     <meta name="keywords" content="unicode emoji characters, utf-8">
 </head>
 <body>
@@ -19,31 +20,35 @@
           <li><a href="#">Contacts</a></li>
         </ul>
     </nav> 
+    
+    <section id="main_login">
+        <section id="search_bar">
+            <form id="search_form" action="search_page.php" method="post"> 
+                <div class="search_box">
+                    <input class="location" name="location" type="text" placeholder="Where to?"/>
+                    <input class="date" name="start_date" type="date" value="2019-11-13" min="2019-11-13">
+                    <input class="date" name="end_date" type="date" value="2019-11-14" min="2019-11-14">
+                    <button class="search_button" type="submit">Search</button>
+                </div>
+            </form>
+        </section>
 
-    <section id="search_bar">
-        <form id="search_form" action="search_page.php" method="post"> 
-            <div class="search_box">
-                <input class="location" name="location" type="text" placeholder="Where to?"/>
-                <input class="date" name="start_date" type="date" value="2019-11-13" min="2019-11-13">
-                <input class="date" name="end_date" type="date" value="2019-11-14" min="2019-11-14">
-                <button class="search_button" type="submit">Search</button>
-            </div>
-        </form>
-    </section>
-
-    <section id="login">
-        <h2>Login</h2>
-        <form id="login_form">
-            <div class="login_box">
-                <label for="username">Username:</label>
-                <input name="username" type="text" placeholder="Username"/>
-                
-                <label for="password">Password:</label>
-                <input name="password" type="password" placeholder="Password"/>
-                
-                <input type="Submit" value="Login">
-            </div>
-        </form>
+        <section id="login">
+            <h2>Login</h2>
+            <form id="login_form">
+                <div class="login_box">
+                    <div class="username">
+                        <label for="username">Username:</label>
+                        <input name="username" type="text" placeholder="Username"/>
+                    </div>
+                    <div class="password">
+                        <label for="password">Password:</label>
+                        <input name="password" type="password" placeholder="Password"/>
+                    </div>
+                    <input class="login_button" type="Submit" value="Login">
+                </div>
+            </form>
+        </section>
     </section>
 
 </body>
