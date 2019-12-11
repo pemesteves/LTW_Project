c6cb2d8c1fc8b214406625e2afade27547f195e5
Nuno <up201706162@fe.up.pt>
2019-12-11 12:23:39 +0000

    Fixed bug

--- pages/index.php
+++ pages/index.php
@@ -53,7 +53,18 @@
             <div class="height_crop">
                 <img src="../images/main_paradise.jpg" alt="Main Page Rntify Image"/>
             </div>
+            <?php 
+                if (isset($_SESSION['username'])) {
+            ?>
             <form id="rentify_form" action="add_properties.php"> 
+            <?php
+                }
+                else {
+            ?>
+                <form id="rentify_form" action="login.php">
+            <?php
+                }
+            ?>
                 <div class="rentify_box">
                     <button class="rentify_button" type="submit">Rentify</button>
                 </div>
