6689377e3ceb8372756c7d343e8b6365f82546e9
Pedro Esteves <up201705160@fe.up.pt>
2019-12-15 19:54:52 +0000

    Added more try...catch

--- includes/init.php
+++ includes/init.php
@@ -1,8 +1,13 @@
  <?php
-  include_once('session.php');
-  include_once('../database/connection.php');
+  include_once 'session.php';
+  include_once '../database/connection.php';
 
   if(!isset($_SERVER['HTTP_REFERER'])){
     $_SERVER['HTTP_REFERER'] = '../pages/index.php';
+  }
+  
+  function catchException($e){
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    die($e->getMessage());
   }
 ?>
